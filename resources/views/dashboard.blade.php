7d008777ddbc41a53e35cbfaaa6a4e72cf561119
Zeb Abbasi <abbasizeb78@gmail.com>
2023-06-21 00:19:38 +0500

    view dashboard acccording to logins

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,95 +3,142 @@
 @section('title', 'Dashboard | Electronic Health Record')
 
 @section('content')
-<div class="container-fluid">
-    <h1 class="mt-5 fw-normal text-uppercase fs-2">Dashboard</h1>
-    @include('common.alert')
-    {{-- @include('common.message-modal') --}}
-    <div class="row mt-5">
+    <div class="container-fluid">
+        <h1 class="mt-5 fw-normal text-uppercase fs-2">Dashboard</h1>
+        @include('common.alert')
+        {{-- @include('common.message-modal') --}}
+        <div class="row mt-5">
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs fw-bold text-primary text-uppercase mb-1">
-                                Update Profile
+            @php
+                // $patient_tiles = ['Profile', 'Appointments', 'Book Appointment'];
+                $patient_tiles = [
+                    [
+                        'title' => 'My profile',
+                        'icon' => 'fa-solid fa-house',
+                        'description' => 'Update Profile',
+                        'url' => '#',
+                    ],
+                    [
+                        'title' => 'My Appointments',
+                        'icon' => 'fas fa-calendar fa-2x text-gray-300',
+                        'description' => ' View Appointment History ',
+                        'url' => '#',
+                    ],
+                    [
+                        'title' => 'Book My Appointment',
+                        'icon' => 'fas fa-calendar-plus fa-2x text-gray-300',
+                        'description' => ' Book Appointment ',
+                        'url' => '#',
+                    ],
+                ];
+                $admin_tiles = [
+                    [
+                        'title' => 'Manage Patients',
+                        'icon' => 'fa-solid fa-house',
+                        'description' => 'Update Profile',
+                        'url' => '#',
+                    ],
+                    [
+                        'title' => 'Manage Doctors',
+                        'icon' => 'fas fa-calendar fa-2x text-gray-300',
+                        'description' => ' View Appointment History ',
+                        'url' => '#',
+                    ],
+                    [
+                        'title' => 'Appointments',
+                        'icon' => 'fas fa-calendar-plus fa-2x text-gray-300',
+                        'description' => ' Book Appointment ',
+                        'url' => '#',
+                    ],
+                ];
+                $doctor_tiles = [
+                    [
+                        'title' => 'My profile',
+                        'icon' => 'fa-solid fa-house',
+                        'description' => 'Update Profile',
+                        'url' => '#',
+                    ],
+                    [
+                        'title' => 'My Appointments',
+                        'icon' => 'fas fa-calendar fa-2x text-gray-300',
+                        'description' => ' View Appointment History ',
+                        'url' => '#',
+                    ],
+                ];
+                $doctor_tiles = ['Profile', 'Appointments'];
+                $admin_tiles = ['Manage Patients', 'Manage Doctors', 'Appointments'];
+            @endphp
+            @if ($user->role_id == 1)
+                @foreach ($admin_tiles as $admin_tile)
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs fw-bold text-primary text-uppercase mb-1">
+                                            {{ $admin_tile }}
+                                        </div>
+                                        <div class="h5 mb-0 fw-bold text-gray-800">My profile</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="h5 mb-0 fw-bold text-gray-800">My profile</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Annual) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs fw-bold text-success text-uppercase mb-1">
-                                View Appointment History</div>
-                            <div class="h5 mb-0 fw-bold text-gray-800">My Appointments</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Tasks Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs fw-bold text-info text-uppercase mb-1">
-                                Book Appointment </div>
-                            <div class="h5 mb-0 fw-bold text-gray-800">Book My Appointment</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                @endforeach
+            @endif
+            @if ($user->role_id == 2)
+                @foreach ($doctor_tiles as $doctor_tile)
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs fw-bold text-primary text-uppercase mb-1">
+                                            {{ $doctor_tile }}
+                                        </div>
+                                        <div class="h5 mb-0 fw-bold text-gray-800">My profile</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                @endforeach
+            @endif
+            @if ($user->role_id == 3)
+                @foreach ($patient_tiles as $patient_tile)
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <a class="text-decoration-none" href="{{ $patient_tile['url'] }}">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs fw-bold text-primary text-uppercase mb-1">
+                                                {{ $patient_tile['description'] }}
+                                            </div>
+                                            <div class="h5 mb-0 fw-bold text-gray-800">{{ $patient_tile['title']}}</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="{{ $patient_tile['icon'] }}"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
 
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs fw-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 fw-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
                     </div>
-                </div>
-            </div>
+                @endforeach
+            @endif
+
+
+
+
         </div>
-    </div>
-</div>
-    {{-- @include('partials.banner')
-
-    @include('partials.logins-section')
-
-    @include('partials.features')
-
-    @include('partials.about-us')
-
-    @include('partials.gallery') --}}
 
 
-@endsection
+    @endsection
--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -3,15 +3,24 @@
     <div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
     <div class="list-group list-group-flush">
         {{-- User / patient --}}
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Book Appointment</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Appointment History</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Medical History</a>
-       
-        {{-- doctor --}}
-        {{-- <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Book Appointment</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Patients</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Search</a> --}}
+        @if ($user->role_id == 1)
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Doctors</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Patients</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Appointment
+                History</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Reports</a>
+        @endif
+        @if ($user->role_id == 2)
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Patients</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Appointment
+                History</a>
+        @endif
+        @if ($user->role_id == 3)
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Book Appointment</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Medical History</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Appointment
+                History</a>
+        @endif
     </div>
 </div>
