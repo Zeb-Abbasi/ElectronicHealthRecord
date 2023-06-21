38105de1b4c7b18e2fe650a1f1e83512dccd5ada
Ameer Hamza <ameer.hamza@septemsystems.com>
2023-06-22 01:10:27 +0500

    latest code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,15 +8,17 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index(){
-        $admin = Auth::guard('admin')->user();
-        $doctor = Auth::guard('doctor')->user();
-        $patient = Auth::guard('patient')->user();
-        if($admin) {
-            return view('dashboard',compact('admin'));
-        } else if($doctor) {
-            return view('dashboard',compact('doctor'));
-        } else if($patient) {
-            return view('dashboard',compact('patient'));
-        }
+        // $admin = Auth::guard('admin')->user();
+        // $doctor = Auth::guard('doctor')->user();
+        // $patient = Auth::guard('patient')->user();
+        // if($admin) {
+        //     return view('dashboard',compact('admin'));
+        // } else if($doctor) {
+        //     return view('dashboard',compact('doctor'));
+        // } else if($patient) {
+        //     return view('dashboard',compact('patient'));
+        // }
+        $user = Auth::user();
+        return view('dashboard',compact('user'));
     }
 }
